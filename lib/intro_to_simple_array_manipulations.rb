8c97e9cef35f54c1a2f8050d304e959e66d90454
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 22:35:37 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(arr, str)
+  arr.push(str)
+end 
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end 
+
+def using_pop(arr)
+  arr.pop 
+end 
+
+def pop_with_args(arr)
+  using_pop(arr)
+  using_pop(arr)
+end 
+
+def using_shift(arr)
+  arr.shift 
+end 
+
+def shift_with_args(arr)
+  using_shift(arr)
+  using_shift(arr)
+end 
+
